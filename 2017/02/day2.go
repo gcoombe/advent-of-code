fefe903da7912404187aefeeba97dcd553f7c967
Graham Coombe <gscoombe@gmail.com>
2017-12-13 10:04:52 -0800

    Finish day2 - go

--- 2017/02/day2.go
+++ 2017/02/day2.go
@@ -20,6 +20,7 @@ func main() {
 	fmt.Println("----------------------------------------")
 
 	fmt.Printf("\nPart 1 res: %d", runPartOne(filepath))
+	fmt.Printf("\nPart 2 res: %d", runPartTwo(filepath))
 
 	fmt.Println()
 	fmt.Println("----------------------------------------")
@@ -28,6 +29,13 @@ func main() {
 
 }
 
+func listStringsToInts(strings []string) []int {
+	nums := make([]int, len(strings))
+	for i, string := range strings {
+		nums[i], _ = strconv.Atoi(string)
+	}
+	return nums
+}
 
 func runPartOne(filePath string) int {
 	file := goUtils.GetFile(filePath)
@@ -42,13 +50,13 @@ func runPartOne(filePath string) int {
 	checksum := 0
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		values := strings.Split(line, "\t")
+		values := listStringsToInts(strings.Split(line, "\t"))
 
-		lowest, _ := strconv.Atoi(values[0])
+		lowest := values[0]
 		highest := lowest
 
 		for i := 1; i < len(values); i++ {
-			value, _ := strconv.Atoi(values[i])
+			value := values[i]
 			if value < lowest {
 				lowest = value
 			} else if value > highest {
@@ -56,6 +64,37 @@ func runPartOne(filePath string) int {
 			}
 		}
 		checksum += highest - lowest
+	}
+
+	return checksum
+}
+
+func runPartTwo(filePath string) int {
+	file := goUtils.GetFile(filePath)
+	if file == nil {
+		return 0
+	}
+
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+
+	checksum := 0
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		values := listStringsToInts(strings.Split(line, "\t"))
+
+		for i, num1 := range values {
+			for _, num2 := range values[i + 1:] {
+				if num1 != 0 && num2 != 0 {
+					if num1 % num2 == 0 {
+						checksum += num1 / num2
+					} else if num2 % num1 == 0 {
+						checksum += num2 / num1
+					}
+				}
+			}
+		}
 	}
 
 	return checksum
--- 2017/02/day2_test.go
+++ 2017/02/day2_test.go
@@ -15,3 +15,13 @@ func TestPart1(t *testing.T) {
 		t.Errorf("Incorrest answer.  Expected %d got %d", 18, checksum)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	filepath := path.Join(path.Dir(filename), "testInput2.txt")
+
+	checksum := runPartTwo(filepath)
+	if (checksum != 9) {
+		t.Errorf("Incorrest answer.  Expected %d got %d", 9, checksum)
+	}
+}
