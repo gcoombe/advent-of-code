47711a9d94890e4b253162ffab9fd68a93cd2f3f
Graham Coombe <gcoombe@zenefits.com>
2018-12-01 20:50:05 -0800

    2018 part 1

---
+++ 2018/01/01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+def find_repeated_frequency(frequency_changes):
+
+    frequency = 0
+    visited = {}
+    i = 0
+    while(True):
+        frequency += frequency_changes[i]
+        if (visited.get(frequency) != None):
+            return frequency
+        visited[frequency] = True
+        i = (i + 1) % len(frequency_changes)
+
+
+def main():
+    with open('input.txt', 'r') as file:
+        frequency_changes = [int(line) for line in file]
+
+    print('Part 1 answer = {}'.format(sum(frequency_changes)))
+    print('Part 2 answer = {}'.format(
+        find_repeated_frequency(frequency_changes)))
+
+
+if __name__ == '__main__':
+    main()
